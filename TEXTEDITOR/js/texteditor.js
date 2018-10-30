b3cfcb6e6678ff60d258ece879f8642131a9d310
Sneha Narasimhan <snehanarasimhan@ymail.com>
2018-10-29 17:42:24 -0700

    US#10Task#32: Support for underlining the text

--- TEXTEDITOR/js/texteditor.js
+++ TEXTEDITOR/js/texteditor.js
@@ -338,6 +338,27 @@ function italicsText() {
       copysuccess = false;
   }
 }
+
+function underline() {
+	var selectedText = "";
+  if (window.getSelection){
+      selectedText = window.getSelection().toString();
+  }
+  strVariable=selectedText;
+  // var to check whether execCommand successfully executed
+  var copysuccess = [];
+  var res;
+  try{
+	 currHtml = document.getElementById("textarea").innerHTML;
+     copysuccess = strVariable.underline(); // executable command to make the selected text as underline.
+	res = currHtml.replace(selectedText, copysuccess)
+	document.getElementById("textarea").innerHTML = res;
+}
+  catch(e){
+      copysuccess = false;
+  }
+	
+}
 function centerAlign(){
   // var to check whether execCommand successfully executed
   var copysuccess;
